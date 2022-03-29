dce7b783fc9c02ff4a130f0729919d5850ac57c6
Quebim <quebim@pop-os.localdomain>
2022-03-29 13:12:03 -0300

    ERC20 token deployment

---
+++ contracts/QBMToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+contract QBMToken is Context, AccessControlEnumerable, ERC20Burnable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor() ERC20("Quebim Token", "QBM") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+
+        _mint(msg.sender, 1 ether);
+    }
+
+    function mint(address to, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()),"QBMToken: Only minter can mint");
+        _mint(to, amount);
+    }
+}
